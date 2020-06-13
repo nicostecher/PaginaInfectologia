56c7e058db1dc2c3af6ae6319d0d2ce053472246
nico stecher <nicostecher1@gmail.com>
2020-06-13 16:12:29 -0300

    header mobile

---
+++ resources/views/header.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/header.css">
+    <title>Header</title>
+</head>
+<body>
+   <div class="contenedor-principal">
+       <header>
+           <div class="titulo-principal">
+               <h4>Catedra de <br> Infectología</h4>
+           </div>
+
+           <div class="menu-mobile">
+            <div class="fixed-top">
+                <div class="collapse" id="navbarToggleExternalContent">
+                  <div class="hamburgesa">
+                <ul class="listado-mobile">
+                    <li class="text-muted">Cronograma</li>
+                    <li class="text-muted">Clases</li>
+                    <li class="text-muted">Casos Clinicos</li>
+                    <li class="text-muted">Cuerpo Docente</li>
+                    <li class="text-muted">Bibliografía</li>
+                  </div>
+                </ul>
+                <form action="">
+                  <label for="">Busqueda</label>
+                  <input type="text">
+                </form>
+                </div>
+                <nav id="importante" class="navbar navbar-dark bg-dark">
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                </nav>
+              </div>
+            </div>
+
+       </header>
+       
+
+   </div>
+   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/header', function(){
+    return view("header");
+});
