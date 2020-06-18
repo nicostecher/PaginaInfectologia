d30ba71823784aafd53500d52e9a93402cad3320
zoe-schmidt <zowi.schmidt@hotmail.com>
2020-06-18 20:31:24 -0300

    css plantilla y empezando cuerpo docente

--- resources/views/cuerpoDocente.blade.php
+++ resources/views/cuerpoDocente.blade.php
@@ -5,10 +5,19 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/cuerpoDocente.css">
     <title>Cuerpo Docente</title>
   </head>
   <body>
-
+    <div class="cuerpo-docente">
+      <div class="imagen-docente">
+        <img src="" alt="">
+      </div>
+      <div class="descripcion docente">
+        <p class="nombre-docente"></p>
+        <p class="cargos-docente"></p>
+      </div>
+    </div>
   </body>
 </html>
 
