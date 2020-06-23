746a67480915ea8a8a254c172cf826d780d0ebd9
nico stecher <nicostecher1@gmail.com>
2020-06-23 20:03:29 -0300

    agregada novedades a la plantilla

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,42 +9,87 @@
     <title></title>
   </head>
   <body>
+
+  
+    <div class="imagen-fondo">
+
     <div class="container">
+
       <div class="barra-busqueda">
-        <nav class="navbar navbar-light bg-light">
+        <nav class="navbar navbar-light bg-light" class="menu-busqueda">
           <a class="navbar-brand" href="#">
             <img class="lupita"src="/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
               <p class="busqueda">Buscar</p>
           </a>
         </nav>
       </div>
+
+      
+
       <div class="novedades">
-        <a href="novedades">Novedades!</a>
-        <div class="noticia1">
-        </div>
-        <div class="noticia2">
-        </div>
-      </div>
+        <a href="novedades"><h6>Novedades!</h6></a>
 
+          <div class="noticia1">
+            <div class="titulo-noticia">
+              <h5>fechas de Parcial</h5>
+            </div>
+
+            <div class="descripcion-noticias">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, rem placeat! Porro praesentium nulla </p>
+            </div>
+
+          </div>
+
+          <div class="noticia2">
+              <div class="titulo-noticia">
+                <h5>Cambios de Clase</h5>
+              </div>
+              
+                <div class="descripcion-noticias">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, maxime necessitatibus placeat, dolore autem voluptatem aut aliquam mollitia, facilis hic quae incidunt dolorum dicta libero iure. Officiis modi maiores fugit. nulla<p>
+                  </div>
+                </div>
+
+
+                <div class="noticia2">
+                  <div class="titulo-noticia">
+                    <h5>Otras Noticias</h5>
+                  </div>
+                  
+                    <div class="descripcion-noticias">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, maxime necessitatibus placeat, dolore autem voluptatem aut aliquam mollitia, facilis hic quae incidunt dolorum dicta libero iure. Officiis modi maiores fugit. nulla<p>
+                      </div>
+                    </div>
+
+
+            
+
+      </div>
+     
+        
       <div class="categorias">
-        <div class="categoria-cronograma">
-          <a href="/cronograma">Cronograma</a>
-        </div>
-        <div class="categoria-bibliografia">
-          <a href="/bibliografia">Bibliografia</a>
-        </div>
-        <div class="categoria-casosClinicos">
-          <a href="/casosClinicos">Casos Clinicos</a>
-        </div>
-        <div class="categoria-clases">
-          <a href="/clases">Clases</a>
-        </div>
-        <div class="categoria-cuerpoDocente">
-          <a href="/cuerpoDocente">Cuerpo Docente</a>
-        </div>
+          <div class="categoria-cronograma">
+            <a href="/cronograma">Cronograma</a>
+          </div>
+          <div class="categoria-bibliografia">
+            <a href="/bibliografia">Bibliografia</a>
+          </div>
+          <div class="categoria-casosClinicos">
+            <a href="/casosClinicos">Casos Clinicos</a>
+          </div>
+          <div class="categoria-clases">
+            <a href="/clases">Clases</a>
+          </div>
+          <div class="categoria-cuerpoDocente">
+            <a href="/cuerpoDocente">Cuerpo Docente</a>
+          </div>
 
       </div>
+
+      <br>
+
     </div>
+  </div>
 
   </body>
 </html>
