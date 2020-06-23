3457a463e4e22a8ceca9f464b1585072ae9e1988
nico stecher <nicostecher1@gmail.com>
2020-06-22 21:51:50 -0300

    arreglada la superposición del menu y mejorado clases y cronograma

--- resources/views/clases.blade.php
+++ resources/views/clases.blade.php
@@ -8,8 +8,13 @@
 <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="/css/header.css">
 <link rel="stylesheet" href="/css/clases.css">
+
   
+
 <div class="contenedor-principal">
+  
+  <div class="transparencia">
+
 
       <div class="titulo-clases">
         <h2>Clases</h2>
@@ -18,43 +23,48 @@
 
       <section class="contenido-clases">
 
-        <div class="titulo-escritorio">
-          <h2>Clases</h2>
-        </div>
+       
 
-        <br>
-
-        <article class="clase">
-
-          <div class="video">
-          <iframe src="https://docs.google.com/viewer?srcid=1v2U0h14MMKsdOXEvX8pgHCN6-OLETX3N&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="80%" height="250px" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="titulo-escritorio">
+              <h2>Clases</h2>
             </div>
 
-            <div class="descripcion">
-              <p>Clase 1</p>
+            <br>
+
+            <article class="clase">
+
+              <div class="video">
+              <iframe src="https://docs.google.com/viewer?srcid=1v2U0h14MMKsdOXEvX8pgHCN6-OLETX3N&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="80%" height="250px" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+                <div class="descripcion">
+                  <p>Clase 1</p>
+                  </div>
+
+            </article>
+
+            <br>
+
+            <article class="clase2">
+              <div class="pdf">
+                <iframe src="https://docs.google.com/viewer?srcid=1QDqahbmx_CWZi_sS7bEfqSz8cKmk9lhS&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="90%" height="250px" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+                
               </div>
-
-        </article>
-
-        <br>
-
-        <article class="clase2">
-          <div class="pdf">
-            <iframe src="https://docs.google.com/viewer?srcid=1QDqahbmx_CWZi_sS7bEfqSz8cKmk9lhS&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="90%" height="250px" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
+              
+                <div class="descripcion">
+                  <p>Clase 2</p>
+                  </div>
+            </article>
             
-          </div>
-          
-            <div class="descripcion">
-              <p>Clase 2</p>
-              </div>
-        </article>
-        
 
       </section>
 
    <br>
+   <br>
+   <br>
 
+    </div>
   </div>
 
 
--- resources/views/cronograma.blade.php
+++ resources/views/cronograma.blade.php
@@ -1,34 +1,40 @@
 @extends("plantilla")
 @section("main")
 
+
+    
+
+
     <meta name="viewport" content="width=device-width, user-scalable=no">
     <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/cronograma.css">
 
     <title>Cronograma</title>
   
-
     
+
+
     <section class="contenedor-cronograma ">
+
+    <div class="transparencia">
+
+        <div class="caja-cronograma">
         
-        <div class="imagen-fondo">
-            
-       
       
           <div class="titulo-cronograma" >
-             <h2> Cronogorama</h2>
-          </div>
+             <h2> Cronograma</h2>
+             </div>
       
-      <div class="imagen-cronograma">
+         <div class="imagen-cronograma">
           <img src="/imagenes/cronograma.jpg"  alt="">
-       </div>
+            </div>
 
-    </div>
+        </div>
+   
     </section>
+    
 
-    <br>
-    <br>
-    <br>
+
 
 
 @endsection
