509bf51969e5bc86df0a0e8540cf593c5673dfe4
nico stecher <nicostecher1@gmail.com>
2020-06-26 18:47:31 -0300

    creada casos clinicos y resuelto detalles de estilo

--- resources/views/casosClinicos.blade.php
+++ resources/views/casosClinicos.blade.php
@@ -1,15 +1,26 @@
 @extends("plantilla")
 @section("main")
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Casos Clinicos</title>
-  </head>
-  <body>
+<link rel="stylesheet" href="/css/casosClinicos.css">
 
-  </body>
-</html>
+<section class="contenedor-casosClinicos">
+  
+  <div class="titulo-casosClinicos">
+      <h2>Casos Clinicos</h2>
+  </div>
 
+
+  <div class="contenido-casosClinicos">
+  
+    <ul  class="listado-casosClinicos">
+      <li>Caso Clinico 1</li>
+      <li>Caso Clinico 2</li>
+      <li>Caso Clinico 3</li>
+      <li>Caso Clinico 4</li>
+      <li>Caso Clinico 5</li>
+    </ul>
+
+    </div>
+
+</section>
 @endsection
